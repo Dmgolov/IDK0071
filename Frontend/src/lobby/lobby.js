287750457e18d0c8c87aa5c7f2541409b6a03d38
imgret <imgret@ttu.ee>
2018-02-24 00:47:06 +0200

    now buttons for adding and substracting points to/from nation attributes are working, however there is no checking for number of used nation points number

--- Frontend/src/lobby/lobby.js
+++ Frontend/src/lobby/lobby.js
@@ -6,10 +6,31 @@ export class Lobby {
       "Player C",
       "Player D"
     ];
+
+    // this.nationAttributes =
+    //   [["Vitality", 0], ["Growth rate", 0]],
+    //   [["Strength", 0], ["Intelligence", 0]],
+    //   [["Dexterity", 0], ["Luck", 0]]
+    // ;
+
     this.nationAttributes = [
-      ["Vitality", "Growth rate"],
-      ["Strength", "Intelligence"],
-      ["Dexterity", "Luck"]
+      [{name: "Vitality", points: 0}, {name: "Growth rate", points: 0}],
+      [{name: "Strength", points: 0}, {name: "Intelligence", points: 0}],
+      [{name: "Dexterity", points: 0}, {name: "Luck", points: 0}]
     ];
+
+    this.nationPoints = 20;
+  }
+
+  changeNationAttributeValue(name, points) {
+    for(let row of this.nationAttributes) {
+      for(let attribute of row) {
+        if(attribute.name === name)
+        {
+          attribute.points += points;
+          console.log(attribute.points + " " + attribute.name);
+        }
+      }
+    }
   }
 }
