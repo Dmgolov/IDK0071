c4929300a67fb9d9d9f0ca50e7a3ab0c894663a9
imgret <imgret@ttu.ee>
2018-02-24 15:29:51 +0200

    in aurelia.json was added dependency to aurelia-fetch-client; added functionality for disabling ready buttons; added function for proper increasing and decreasing nation points by player; added function for creating json string with player options data

--- Frontend/src/lobby/lobby.js
+++ Frontend/src/lobby/lobby.js
@@ -1,3 +1,5 @@
+import {HttpClient, json} from "aurelia-fetch-client";
+
 export class Lobby {
   constructor() {
     this.players = [
@@ -7,30 +9,45 @@ export class Lobby {
       "Player D"
     ];
 
+    this.authPlayer = 'Player C';  // here will be written authenticated player
+
+    this.nationPoints = 20;
+
+    this.nationAttributes = [
+      [{name: "Vitality", points: 0}, {name: "Reproduction", points: 0}],
+      [{name: "Strength", points: 0}, {name: "Intelligence", points: 0}],
+      [{name: "Dexterity", points: 0}, {name: "Luck", points: 0}]
+    ];
+
     // this.nationAttributes =
     //   [["Vitality", 0], ["Growth rate", 0]],
     //   [["Strength", 0], ["Intelligence", 0]],
     //   [["Dexterity", 0], ["Luck", 0]]
     // ;
-
-    this.nationAttributes = [
-      [{name: "Vitality", points: 0}, {name: "Growth rate", points: 0}],
-      [{name: "Strength", points: 0}, {name: "Intelligence", points: 0}],
-      [{name: "Dexterity", points: 0}, {name: "Luck", points: 0}]
-    ];
-
-    this.nationPoints = 20;
   }
 
   changeNationAttributeValue(name, points) {
     for(let row of this.nationAttributes) {
       for(let attribute of row) {
-        if(attribute.name === name)
-        {
-          attribute.points += points;
-          console.log(attribute.points + " " + attribute.name);
+        if(attribute.name === name &&
+          attribute.points + points >= 0 &&
+          this.nationPoints - points >= 0) {
+            attribute.points += points;
+            this.nationPoints -= points;
         }
       }
     }
+  }
+
+  sendReadyStateInfo(player) {
+    let client = new HttpClient();
+
+    let info = {player: this.authPlayer, nationAttributes: new Array()};
+    for(let row of this.nationAttributes) {
+      for(let attribute of row) {
+        info.nationAttributes.push(attribute);
+      }
+    }
+    console.log(json(info));
   }
 }
