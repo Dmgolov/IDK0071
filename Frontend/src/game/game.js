76213914ef16c26796dc02067077cb8d6e0c2c28
dmgolo <dmgolo@ttu.ee>
2018-02-28 11:08:24 +0200

    Created 2d array for nation.

--- Frontend/src/game/game.js
+++ Frontend/src/game/game.js
@@ -8,6 +8,9 @@ export class Game {
       "Player 4"
     ];
     this.fillCell = 0.5;
+    let cells = [];
+    console.log();
+    console.log(cells);
   }
 
   updateMap(){
@@ -27,7 +30,22 @@ export class Game {
     const context = this.gameMap.getContext('2d');
     var mapSizeX = 901;
     var mapSizeY = 501;
-    var cellSize = 4;
+    var cellSize = 50;
+    let cells = [];
+    for (let i = 0; i < mapSizeX - 1; i += cellSize) {
+      let row = new Array();
+      for (let j = 0; j < mapSizeY - 1; j += cellSize) {
+        // cells.push({
+        //   x: i,
+        //   y: j,
+        //   nation: "Green"
+        // });
+        row.push({x: i, y: j, nation: "Green"});
+
+      }
+      cells.push(row);
+    }
+    console.log(cells);
     for (var x = 0.5; x < mapSizeX; x += cellSize) {
       for (var y = 0.5; y < mapSizeY; y += cellSize) {
       context.moveTo(x, 0);
@@ -37,19 +55,14 @@ export class Game {
     }
       context.stroke();
     }
-    setInterval(function () {
-      const context = this.gameMap.getContext('2d');
-      var mapSizeX = 901;
-      var mapSizeY = 501;
-      var cellSize = 4;
-      var randomX = Math.random()*(901 - 0.5) + 0.5;
-      var randomY = Math.random()*(501 - 0.5) + 0.5;
-      context.rect(randomX,randomY,3,3);
-      context.fill();
-      context.stroke();
-      this.fillCell += 1;
-      console.log(this.cells);
-    }, 10);
+    //setInterval(this.updateMap(), 10);
+    for (let row of cells){
+      for (let cell of row){
+        if (cell.nation == "Green"){
+          console.log("IS GREEN PLACE");
+        }
+      }
+    }
   }
   fullRandom() {
 
