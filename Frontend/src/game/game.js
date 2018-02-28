546c580bd7ba818d6801e191cc4dec45ae6ae313
dmgolo <dmgolo@ttu.ee>
2018-02-28 15:04:35 +0200

    Detele unncessary path.

--- Frontend/src/game/game.js
+++ Frontend/src/game/game.js
@@ -16,42 +16,6 @@ export class Game {
     };
   }
 
-
-  // updateMap() {
-  //   const context = this.gameMap.getContext('2d');
-  //   var mapSizeX = 901;
-  //   var mapSizeY = 501;
-  //   var cellSize = 4;
-  //   var randomX = Math.random()*(901 - 0.5) + 0.5;
-  //   var randomY = Math.random()*(501 - 0.5) + 0.5;
-  //   context.rect(randomX,randomY,20,20);
-  //   context.fill();
-  //   context.stroke();
-  //   this.fillCell += 1;
-  // }
-
-  // createMap() {
-  //   var mapSizeX = 901;
-  //   var mapSizeY = 501;
-  //   var cellSize = 1;
-  //   let cells = [];
-  //   let nation = "";
-  //   let nationColor = "";
-  //   for (let i = 0; i < mapSizeX - 1; i += cellSize) {
-  //     let row = new Array();
-  //     for (let j = 0; j < mapSizeY - 1; j += cellSize) {
-  //       if (Math.random() < 0.5){
-  //         row.push({x: i, y: j, nation: "green", nationColor: "#0F0"});
-  //       } else {
-  //         row.push({x: i, y: j, nation: "red", nationColor: "#F00"});
-  //       }
-  //     }
-  //     cells.push(row);
-  //   }
-  //   console.log(cells);
-  //   this.cells = cells;
-  // }
-
   attached(){
     this.map.context = this.gameMap.getContext('2d');
     let cells = this.createNations(this.map.width, this.map.height, this.map.cellSize);
@@ -77,16 +41,11 @@ export class Game {
     for (let row of cells){
       for (let cell of row){
         if (cell.nation == "red"){
-          //console.log("THIS PLACE BELONG TO " + cell.nation);
           context.fillStyle = cell.nationColor;
           context.fillRect(cell.x, cell.y, cellSize, cellSize);
-          // context.fill();
         } else if (cell.nation == "green") {
           context.fillStyle = cell.nationColor;
           context.fillRect(cell.x, cell.y, cellSize, cellSize);
-          // context.rect(cell.x, cell.y, cellSize, cellSize);
-          // context.fillStyle = cell.nationColor;
-          // context.fill();
         }
       }
     }
@@ -103,7 +62,6 @@ export class Game {
           row.push({x: i, y: j, nation: "green", nationColor: "#0F0"});
         } else {
           row.push({x: i, y: j, nation: "red", nationColor: "#F00"});
-
         }
       }
       cells.push(row);
