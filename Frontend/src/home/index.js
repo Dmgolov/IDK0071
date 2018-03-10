8553440a0d24d99e4f501eb7115e04f7b87bc6ce
Imre Gretsi <imgret@ttu.ee>
2018-03-10 13:16:11 +0200

    started work on multiplayer mode; now button "connect to lobby" is working and creation of multiplayer game from lobby is also working, however there is a bug with GameMap object initialization, which must be fixed until next commit.

--- Frontend/src/home/index.js
+++ Frontend/src/home/index.js
@@ -28,8 +28,34 @@ export class Home {
       .then(data => {
         this.lobbyInfo.lobbyId = data.lobbyId;
         console.log(this.lobbyInfo);
-        
+
         this.router.navigate("lobby");
+    });
+  }
+
+  connectToLobby() {
+    console.log(this.lobbyInfo);
+
+    let client = new HttpClient();
+    client.fetch("http://localhost:8080/lobby/connect", {
+      "method": "POST",
+      "body": json({
+        "playerName": this.lobbyInfo.playerName,
+        "lobbyId": this.lobbyInfo.lobbyId
+      }),
+      headers: {
+        'Origin': 'http://localhost:8080',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("this was connect response");
+        console.log(data);
+        if (data.status !== "failed") {
+          this.lobbyInfo.gameMode = data.gameMode;
+          this.router.navigate("lobby");
+        }
     });
   }
 }
