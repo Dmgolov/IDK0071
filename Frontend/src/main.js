cf86aec131575adf01e95c382775946fe31a313b
imgret <imgret@ttu.ee>
2018-03-27 20:49:57 +0300

    basic config for spoonX auth plugin was made, but it is not fully done yet

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,8 +1,15 @@
 import environment from './environment';
+import authConfig from './authConfig';
 
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
+    .plugin('aurelia-api', config => {
+      config.registerEndpoint('auth');
+    })
+    .plugin('aurelia-authentication', baseConfig => {
+        baseConfig.configure(authConfig);
+    })
     .feature('resources');
 
   if (environment.debug) {
