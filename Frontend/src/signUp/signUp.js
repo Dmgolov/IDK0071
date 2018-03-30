c35f1d134cb8a8def640cc5935a18329450ff05e
imgret <imgret@ttu.ee>
2018-03-30 21:12:44 +0300

    added implementation of sign in, and sign up, and sign out; however it is not used in lobby and game yet

--- Frontend/src/signUp/signUp.js
+++ Frontend/src/signUp/signUp.js
@@ -2,9 +2,29 @@ import {inject} from "aurelia-framework";
 import {HttpClient, json} from "aurelia-fetch-client";
 import {Router} from 'aurelia-router';
 import {UtilityInfo} from "../utility/utilityInfo";
+import {AuthService} from 'aurelia-authentication';
 
-@inject(UtilityInfo, Router)
+@inject(UtilityInfo, Router, AuthService)
 export class SignUp {
-  constructor(utilityInfo, router) {
+  constructor(utilityInfo, router, authService) {
+    this.utilityInfo = utilityInfo;
+    this.router = router;
+    this.authService = authService;
+
+    this.username;
+    this.email;
+    this.password;
   }
+
+  signUp() {
+    this.authService.signup(this.username, this.email, this.password)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+          console.log("registration failure");
+          console.log(err);
+      });
+  }
+
 }
