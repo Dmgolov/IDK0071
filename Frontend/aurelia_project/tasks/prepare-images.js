a0bdb14538a7d17fdc2271f376e6ea6def4f3f46
imgret <imgret@ttu.ee>
2018-04-06 18:42:53 +0300

    changed name in task->prepare-images.js from prepareFonts to prepareImages

--- Frontend/aurelia_project/tasks/prepare-images.js
+++ Frontend/aurelia_project/tasks/prepare-images.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import changedInPlace from 'gulp-changed-in-place';
 import project from '../aurelia.json';
 
-export default function prepareFonts() {
+export default function prepareImages() {
 
   const taskFonts = gulp.src('src/resources/images/*.{gif,jpg,jpeg,png}')
     .pipe(changedInPlace({ firstPass: true }))
