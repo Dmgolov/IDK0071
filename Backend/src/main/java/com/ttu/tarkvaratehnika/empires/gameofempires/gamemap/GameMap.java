f06e34b1593d5657a6791fe68435d5e52dc56e49
dmgolo <dmgolo@ttu.ee>
2018-04-10 17:50:56 +0300

    Can map select

--- Backend/src/main/java/com/ttu/tarkvaratehnika/empires/gameofempires/gamemap/GameMap.java
+++ Backend/src/main/java/com/ttu/tarkvaratehnika/empires/gameofempires/gamemap/GameMap.java
@@ -1,30 +1,15 @@
 package com.ttu.tarkvaratehnika.empires.gameofempires.gamemap;
 
 // TODO: find a way to set different landscapes to different maps
-public enum GameMap {
-    BASIC_MAP("basic", 100, 100),
-    LARGE_MAP("large", 60, 60),
-    EARTH("earth", 300, 200);
+public class GameMap {
 
-    private String mapName;
-    private int mapWidth;
-    private int mapHeight;
+    private String imageName;
 
-    GameMap(final String mapName, final int mapWidth, final int mapHeight) {
-        this.mapName = mapName;
-        this.mapWidth = mapWidth;
-        this.mapHeight = mapHeight;
+    GameMap(String imageName) {
+        this.imageName = imageName;
     }
 
     public String getMapName() {
-        return mapName;
-    }
-
-    public int getMapWidth() {
-        return mapWidth;
-    }
-
-    public int getMapHeight() {
-        return mapHeight;
+        return imageName;
     }
 }
